7995741fe7cbdbe67fb8b24ca39d3045de7797d2
Seth Kintigh <skintigh@gmail.com>
2020-09-02 13:03:54 -0400

    initial commit

---
+++ parse_decode.py
@@ -0,0 +1,34 @@
+import re
+import sys
+
+lst = []
+max_list = 30  #128 and 256 find bad data
+min_match = 30 #10 found false positives, so did 20
+
+with open("temp.txt", 'r') as infile:
+	while True:
+		line = infile.read(1000000)
+		if not line: break
+		for ml in re.finditer('4 4 4 0 4 0 4 0 2 4 6 0 0 0 0 0 6 4 2 0 0 0 4 4 2 7 7 4 6 7 5 6 .{147}', line): #read by line for huge files, works?
+			found = False
+			m = ml.group(0)
+			#print(m)
+			for x in range(0, len(lst)):
+				if lst[x][0] == m:			#found so break after
+					found == True
+					lst[x][1] += 1
+					if lst[x][1]==min_match:
+						print(m)
+						#sys.stdout.flush()
+					break
+			if not found:
+				#print(m,"not found")
+				lst.append([m,1])
+			#if len(lst) == 15:
+			#	break
+			if len(lst)>max_list:
+				#print ("too long", len(lst))
+				lst=lst[-max_list:-1]
+			#print(len(lst))
+
+	#print(lst)
