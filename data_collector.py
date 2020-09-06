ceeeef8485345e67d083b666d170a123bc34042d
Seth Kintigh <skintigh@gmail.com>
2020-09-06 16:54:57 -0400

    improved reliability when reads fail, write to disk in long collections, added more bytes and multibyte collecting

--- data_collector.py
+++ data_collector.py
@@ -10,7 +10,7 @@ def read_symbols():
 	lst = []
 	max_list = 30  #128 and 256 find bad data
 	min_match = 30 #10 found false positives, so did 20
-	#last = None
+	last = None
 	with open("symbols.txt", 'r') as infile:
 		infile.seek(0,2)
 		end = infile.tell()
@@ -47,31 +47,38 @@ def get_data_symbols(prev_data, n=0):
 	if n == 20:
 		print("get_data_symbols failure n ==", n)
 		sys.exit(1)
-	data = None
+	#data = None
+	data = read_symbols()
 	while data == None:
-		#print(".",end='')
+		print(".",end='')
+		sys.stdout.flush()
 		#sys.stdout.flush()
+		sleep(0.1)
 		data = read_symbols()
+	#data = read_symbols()
 	#print(data)
 	data = data.split('4 4 4 0 4 0 4 0 2 4 6 0 0 0 0 0 6 4 2 0 0 0 4 4 2 7 7 4 6 7 5 6 ')[1] #chop header
 	data = [int(s) for s in data.split(' ')] #convert to list
 	data = data[4*5:4*16] #return just user data portion
 	
-	if data == prev_data:
+	if data == prev_data or data == None:
 		sleep(0.1)
 		data = get_data_symbols(prev_data,n+1)
 	return data
 
 
-def update_packet(a,b=0,c=0,d=0,debug=False):
-	str = 'u {0:02x}{1:02x}{2:02x}{3:02x}0000000000'.format(a,b,c,d)
+def update_packet(a,b=0,c=0,d=0,e=0,f=0,debug=False):
+	str = 'u {0:02x}{1:02x}{2:02x}{3:02x}{4:02x}{5:02x}000000'.format(a,b,c,d,e,f)
 	if debug: print("update_packet set to", str)
 	ser.write(str.encode())
-	sleep(0.1)
+	sleep(0.10)
 	ser.write(b'y\r\n')
+	#sleep(0.15)
 	r = ser.read(1000).decode('ascii')
+	if debug: print(r)
 	if "Loading Data:" not in r:
 		print("update_packet: error with command to write data")
+		print('"Loading Data" not in', r)
 		sys.exit()
 	#print(r)
 	sleep(0.4)	#0.3 fails
@@ -87,36 +94,46 @@ def main(debug=False):
 	if "U <hex bytes>: Update transmit packet" not in r:
 		print("Serial port failure")
 		sys.exit(1)
+
+
 	
-	update_packet(0xff,0xff,0xff,0xff)
+	update_packet(0,0,255,0xff,0xff,0xff)
 	symbols = get_data_symbols(0)
-	if symbols != [3, 7, 6, 2, 4, 4, 5, 4, 6, 2, 1, 5, 2, 6, 7, 6, 4, 3, 0, 1, 7, 4, 0, 2, 0, 7, 6, 6, 1, 6, 4, 1, 3, 1, 4, 4, 0, 7, 7, 3, 5, 2, 7, 7]:
+	#print(symbols)
+	expected = [3, 7, 6, 2, 4, 4, 5, 4, 6, 2, 1, 5, 2, 6, 7, 6, 4, 3, 0, 1, 7, 4, 0, 2, 0, 7, 6, 6, 1, 6, 4, 1, 3, 1, 4, 4, 0, 7, 7, 3, 5, 2, 7, 7]
+	if symbols != expected:
 		print("Error setting symbols to 0xFFFFFFFF")
-		sys.exit(1)
+		print("Expected:",expected)
+		print("Received:",symbols)
+		#sys.exit(1)
+
+
 
 	
-	a=b=d=0
+	a=b=d=e=f=0
 	for c in range(0, 256):
-		update_packet(0,0,c,0)
-		symbols = get_data_symbols(symbols)
-		if debug: 
-			print(c, symbols[0:16])
-		else:
-			print(c, end=' ')
-			sys.stdout.flush() #need this when no \r\n, other times too?
-		db.append([[a, b, c, d],symbols])
-		
+		for d in range(0, 256):
+			update_packet(0,0,c,d,0,0)
+			symbols = get_data_symbols(symbols)
+			if debug: 
+				print(c, symbols[0:16])
+			else:
+				print("%02x%02x"%(c,d), end=' ')
+				sys.stdout.flush() #need this when no \r\n, other times too?
+			db.append([[a, b, c, d, e, f],symbols])
+			
+		#write ever 256 just in case
+		with open("data_2bytes.txt", 'w') as outfile:
+			outfile.write('[\n')
+			for l in db: 
+				outfile.write(str(l)+",\n")
+			outfile.write(']\n')		
 
-	print('\n\n[')
-	for l in db: print(l,end=",\n")
-	print(']')
+	#print('\n\n[')
+	#for l in db: print(l,end=",\n")
+	#print(']')
 	
-	with open("data.txt", 'w') as outfile:
-		outfile.write('[\n')
-		for l in db: 
-			#outfile.write("[" + l* + "],\n")
-			outfile.write(str(l)+",\n")
-		outfile.write(']\n')
+
 	
 		#print(*db, sep=', ', end=']\n')
 
