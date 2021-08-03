79c902208b580a12a2be76a5aa029b77641aa01e
Seth Kintigh <skintigh@gmail.com>
2021-08-03 12:19:45 -0400

    added stage 2, comments

--- craft_win_DC27_badge_game.sh
+++ craft_win_DC27_badge_game.sh
@@ -1,5 +1,6 @@
 # This script creates creates the 5 magic packets for SPEAKER VILLAGE CONTEST ARTIST GOON to win the badge game
 # Note: the badge source code also indicates you need to pair with an UBER badge, but seems to set that game_flags bit the first time it pairs with *any* badge
+# I haven't verified this wins the game yet.
 
 set -e
 HUMAN=0
@@ -16,7 +17,9 @@ UBER=9
 NOT_MAGIC=d0
 MAGIC=d1
 
-
+#stage 1: magic
+#case D  uber/any?
+#case E
 echo -e Creating SPEAKER magic packet
 python write_signal.py -q  1 0 0 0  8   0 1 2 3  $SPEAKER $MAGIC 0 0   0 0 0
 ./craft_multipart_attack.sh
@@ -24,7 +27,7 @@ python write_signal.py -q  1 0 0 0  8   0 1 2 3  $SPEAKER $MAGIC 0 0   0 0 0
 #sox synthetic_signal.wav recordings/silence_1000ms.wav synthetic_signal_win_DC27_game.wav
 sox synthetic_signal.wav synthetic_signal_win_DC27_game.wav
 
-
+#case F
 echo -e Creating VILLAGE magic packet
 python write_signal.py -q  1 0 0 0  8   0 1 2 3  $VILLAGE $MAGIC 0 0   0 0 0
 ./craft_multipart_attack.sh
@@ -32,8 +35,7 @@ sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav temp/out.wav #no del
 #sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav # 1s delay
 mv temp/out.wav synthetic_signal_win_DC27_game.wav
 
-
-
+#case C
 echo -e Creating CONTEST magic packet
 python write_signal.py -q  1 0 0 0  8   0 1 2 3  $CONTEST $MAGIC 0 0   0 0 0
 ./craft_multipart_attack.sh
@@ -41,8 +43,7 @@ sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav temp/out.wav #no del
 #sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav # 1s delay
 mv temp/out.wav synthetic_signal_win_DC27_game.wav
 
-
-
+#case O
 echo -e Creating ARTIST magic packet
 python write_signal.py -q  1 0 0 0  8   0 1 2 3  $ARTIST $MAGIC 0 0   0 0 0
 ./craft_multipart_attack.sh
@@ -50,14 +51,35 @@ sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav temp/out.wav #no del
 #sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav # 1s delay
 mv temp/out.wav synthetic_signal_win_DC27_game.wav
 
-
-
+#case N
 echo -e Creating GOON magic packet
 python write_signal.py -q  1 0 0 0  8   0 1 2 3  $GOON $MAGIC 0 0   0 0 0
 ./craft_multipart_attack.sh
 sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav temp/out.wav #no delay
 #sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav # 1s delay
 mv temp/out.wav synthetic_signal_win_DC27_game.wav
+
+
+#stage 2: don't have to be magic (1 of each color?)
+#human/contest/artist/cfp/uber x
+#goon x
+#speaker x
+#vendor 
+#press
+echo -e Creating VENDOR NON-MAGIC packet
+python write_signal.py -q  1 0 0 0  8   0 1 2 3  $VENDOR $NON-MAGIC 0 0   0 0 0
+./craft_multipart_attack.sh
+sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav temp/out.wav #no delay
+#sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav # 1s delay
+mv temp/out.wav synthetic_signal_win_DC27_game.wav
+
+echo -e Creating PRESS NON-MAGIC packet
+python write_signal.py -q  1 0 0 0  8   0 1 2 3  $PRESS $NON-MAGIC 0 0   0 0 0
+./craft_multipart_attack.sh
+sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav temp/out.wav #no delay
+#sox synthetic_signal_win_DC27_game.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav # 1s delay
+mv temp/out.wav synthetic_signal_win_DC27_game.wav
+
 
 
 #This .wav is fine to play on repeat forever.
--- craft_win_DC27_badge_game_on_hacked_NFMI_FW.sh
+++ craft_win_DC27_badge_game_on_hacked_NFMI_FW.sh
@@ -1,6 +1,7 @@
 # This script creates creates the magic packets for SPEAKER VILLAGE CONTEST ARTIST GOON to win the badge game
 # This script creates 10 packets which become 5 packets in badges with my NFMI firmware hack
 # Note: the badge source code also indicates you need to pair with an UBER badge, but seems to set that game_flags bit the first time it pairs with *any* badge
+# I haven't verified this wins the game yet.
 
 set -e
 HUMAN=0
@@ -21,6 +22,7 @@ MAGIC=d1
 BEGIN=0x42
 END=0x45
 
+#stage 1: magic
 
 echo -e Creating SPEAKER magic packets
 #first 4 bytes of packet
@@ -84,8 +86,30 @@ sox synthetic_signal_win_DC27_game_on_hacked_NFMI_FW.wav temp/part1.wav recordin
 mv temp/out.wav synthetic_signal_win_DC27_game_on_hacked_NFMI_FW.wav
 
 
+#stage 2: doesn't need to be magic
+
+#VENDOR
+echo -e Creating GOON magic packets
+python write_signal.py -q  1 0 0 0  9   $BEGIN  d0 d0  d0 d1  d0 d2  d0 d3   0 0
+./craft_multipart_attack
+mv synthetic_signal.wav temp/part1.wav
+python write_signal.py -q  1 0 0 0  9   0 $VENDOR  0 $NON-MAGIC  0 0  0 0   $END   0 0
+./craft_multipart_attack
+
+sox synthetic_signal_win_DC27_game_on_hacked_NFMI_FW.wav temp/part1.wav recordings/silence_1000ms.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav
+mv temp/out.wav synthetic_signal_win_DC27_game_on_hacked_NFMI_FW.wav
 
 
+#PRESS
+echo -e Creating GOON magic packets
+python write_signal.py -q  1 0 0 0  9   $BEGIN  d0 d0  d0 d1  d0 d2  d0 d3   0 0
+./craft_multipart_attack
+mv synthetic_signal.wav temp/part1.wav
+python write_signal.py -q  1 0 0 0  9   0 $PRESS  0 $NON-MAGIC  0 0  0 0   $END   0 0
+./craft_multipart_attack
+
+sox synthetic_signal_win_DC27_game_on_hacked_NFMI_FW.wav temp/part1.wav recordings/silence_1000ms.wav synthetic_signal.wav recordings/silence_1000ms.wav temp/out.wav
+mv temp/out.wav synthetic_signal_win_DC27_game_on_hacked_NFMI_FW.wav
 
 #add trailing silence if you want this to be a 1 shot attack instead of looped. Looping probably creates half packet that crash badges, better to play once and then hit "r"
 # (really only needs to be 1281ms of silence with previous 1000ms already added)
